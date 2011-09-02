75ce87388a28b2ec11bfc46be927e231a0c369da
Brent S.A. Cowgill <brent.cowgill@ft.com>
2011-09-02 21:14:31 +0100

    Release 1.2 - take 2 - added a replace.pl script to convert existing qunit tests to use QUnitChainer

--- release/make.sh
+++ release/make.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 rm release/QUnitChainer.tar.gz
-tar cvzf release/QUnitChainer.tar.gz README 0-control.html QUnitChainer.js qunitchainer.css  sample/ jquery/ json2/ qunit/ 
+tar cvzf release/QUnitChainer.tar.gz README 0-control.html QUnitChainer.js qunitchainer.css  replace.pl sample/ jquery/ json2/ qunit/
