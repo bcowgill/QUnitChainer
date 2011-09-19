f0b3652a9b355486f11ab4ac895614ef86beb1bb
brent.cowgill <brent.cowgill@ft.com>
2011-09-19 01:04:26 +0100

    correcting output to show URL correctly

--- release/jscover-server.sh
+++ release/jscover-server.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo Starting jscoverage server.
-echo Go to http://127.0.0.1:8080/jscoverage.html?missing=true&frame=spec/SpecRunner.html
+echo 'Go to http://127.0.0.1:8080/jscoverage.html?missing=true&frame=spec/SpecRunner.html'
 echo To run the tests and gather coverage.
 echo Click the Store Tab and Store Response button to save the coverage to jscoverage-report/jscoverage.html
-echo Then view the coverage results at http://sandbox.bsac.com/QUnitChainer/jscoverage-report/jscoverage.html
+echo 'Then view the coverage results at http://sandbox.bsac.com/QUnitChainer/jscoverage-report/jscoverage.html'
 
 jscoverage/jscoverage-server --verbose \
    --no-instrument=json2 --no-instrument=qunit --no-instrument=sample --no-instrument=sinon \
