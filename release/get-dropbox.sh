06d327652db6304fda76684bf7118357c2911c5b
Brent S.A. Cowgill <brent.cowgill@ft.com>
2011-10-14 23:22:17 +0100

    Release 1.6.1 - some cleanup after running files through a validator

--- release/get-dropbox.sh
+++ release/get-dropbox.sh
@@ -6,8 +6,13 @@ if [ -f get-dropbox.sh ]; then
    exit 1
 fi
 tar xvzf $FROM/QUnitChainer.tgz
+chmod -R +w .
+perl -i.bak -pne "s{\s+? \n}{\n}xms; s{(\'nextTestPlan\': \s*) .+? (// \s* REPLACE)}{\$1\'../sample/q-test.html\', \$2}xms" spec/QUnitChainerSpec.js
+
 pushd ../Qucumber
 tar xvzf $FROM/Qucumber.tgz
+chmod -R +w .
 git status
 popd
+
 git status
